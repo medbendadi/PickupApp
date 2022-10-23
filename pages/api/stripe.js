01956ca2ef2ba7e62afc7bdc1f9d7072e636d22e
medbendadi <itsbendadi@gmail.com>
2022-10-23 16:11:59 +0100

    Add Stripe

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -29,8 +29,8 @@ export default async function handler(req, res) {
                   minimum: 1,
                },
                quantity: 1,
-               success_url: `${req.headers.origin}/success`,
-               cancel_url: `${req.headers.origin}/cancel`,
+               success_url: `https://pickup-self.vercel.app/success`,
+               cancel_url: `https://pickup-self.vercel.app/cancel`,
             }
          }
 
