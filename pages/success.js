73fd6433338a33797646ba5fad299d48b46dba6c
medbendadi <itsbendadi@gmail.com>
2022-10-24 01:13:56 +0100

    editing on succes.js

--- pages/success.js
+++ pages/success.js
@@ -3,17 +3,19 @@ import { useState, useEffect } from 'react'
 import { carList } from '../data/carList'
 import { cycleList } from '../data/cycleList'
 const Success = () => {
-   // const router = useRouter()
-   // const { username, type } = router.query
-   // const [data, setData] = useState([])
-   // useEffect(() => {
-   //    if (!(username && type)) return router.replace('/')
-   //    if (type === 'cycling') {
-   //       setData(cycleList)
-   //    } else {
-   //       setData(carList)
-   //    }
-   // }, [])
+   const router = useRouter()
+   const { username, type } = router.query
+   const [data, setData] = useState([])
+   useEffect(() => {
+      if (!(username && type)) return router.replace('/')
+      if (type === 'cycling') {
+         setData(cycleList)
+      } else {
+         setData(carList)
+      }
+   }, [])
+
+   console.log(data);
 
 
    return (
